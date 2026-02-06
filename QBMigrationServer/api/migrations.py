37e246a52bc8bc4f0a42483b337a5fc888c8dd5e
Sivaharan <sivaharanj@gmail.com>
2026-02-05 19:45:47 -0500

    migrations.py change

--- QBMigrationServer/api/migrations.py
+++ QBMigrationServer/api/migrations.py
@@ -1,5 +1,6 @@
 import logging
 import re
+import os
 from datetime import datetime, timezone
 
 from api.auth import require_auth
@@ -145,7 +146,7 @@ def create_migration():
         raw_file_name = files[0] if files else ""
         safe_file_name = sanitize_string(raw_file_name, max_length=255)
         company_name = (
-            safe_file_name.rsplit(".", 1)[0] if safe_file_name else "Unknown Company"
+              os.path.splitext(safe_file_name)[0] if safe_file_name else "Unknown Company"
         )
 
         migration = Migration(
